cc2a07dfae5efa19b692230ca2e56df47b8e94cc
jdeon <jd.eymann@gmail.com>
2025-06-14 23:09:32 +0200

    fix: still use jquery with app v2

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let isChatTab = false;
 ********************************/
 
 Hooks.once('setup', function () {
-    console.log(`${s_MODULE_ID} | setup to ${s_MODULE_ID}`); 
+    console.log(`${s_MODULE_ID} | setup to ${s_MODULE_ID}`);
 
     game.settings.register(s_MODULE_ID, 'minimalRoleToPinnedOther', {
         name: game.i18n.localize('PCM.settings.minimalRole.name'),
@@ -22,7 +22,7 @@ Hooks.once('setup', function () {
             (accumulator, [label, id]) => {
                 const capLabel = label[0].toUpperCase() + label.slice(1).toLowerCase()
                 const localizeLabel = game.i18n.localize(`USER.Role${capLabel}`)
-                accumulator[id] = localizeLabel; 
+                accumulator[id] = localizeLabel;
                 return accumulator
             },
             {}
@@ -68,7 +68,7 @@ Hooks.once('setup', function () {
 })
 
 Hooks.once('ready', function () {
-    console.log(`${s_MODULE_ID} | ready to ${s_MODULE_ID}`); 
+    console.log(`${s_MODULE_ID} | ready to ${s_MODULE_ID}`);
 
     getCurrentTab().addClass('active')
 
@@ -79,15 +79,17 @@ Hooks.once('ready', function () {
 
 //Add chatlog type navigation
 Hooks.on("renderChatLog", async function (chatLog, html, user) {
+    if (chatLog instanceof foundry.applications.api.ApplicationV2) html = $(chatLog.element);
+
     initTab(html, chatLog)
 
     //Check the activation of chat tab
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
             const currentClassState = mutation.target.classList.contains('active')
-            if(!currentClassState){
+            if (!currentClassState) {
                 isChatTab = false
-            } else if(!isChatTab && currentClassState){
+            } else if (!isChatTab && currentClassState) {
                 //If we active the chat Tab we add the activation class on sub class
                 isChatTab = true
                 getCurrentTab().addClass('active');
@@ -98,24 +100,24 @@ Hooks.on("renderChatLog", async function (chatLog, html, user) {
     observer.observe(html[0], {
         attributes: true,
         attributeFilter: ['class']
-      });
+    });
 });
 
 Hooks.on("renderChatMessage", (chatMessage, html, data) => {
     const buttonDisable = !game.user.isGM
-        &&  game.settings.get(s_MODULE_ID, "disablePinForAll")
-        &&  game.settings.get(s_MODULE_ID, "disableSelfPin")
+        && game.settings.get(s_MODULE_ID, "disablePinForAll")
+        && game.settings.get(s_MODULE_ID, "disableSelfPin")
 
-    const allowMessageUpdate = chatMessage.canUserModify(Users.instance.current,'update') 
+    const allowMessageUpdate = chatMessage.canUserModify(Users.instance.current, 'update')
         || game.user.role >= game.settings.get(s_MODULE_ID, "minimalRoleToPinnedOther")
 
-    if(!buttonDisable && allowMessageUpdate){
+    if (!buttonDisable && allowMessageUpdate) {
         addPinnedButton(html, chatMessage);
     }
 
-    if(checkIsPinned(chatMessage) !== ENUM_IS_PINNED_VALUE.none){
+    if (checkIsPinned(chatMessage) !== ENUM_IS_PINNED_VALUE.none) {
         const htmlMessage = $("#chat-log").find(`.${CLASS_PINNED_TAB_MESSAGE}[data-message-id="${chatMessage.id}"]`)
-        if(htmlMessage.length){
+        if (htmlMessage.length) {
             //Already generate message in pinned tab
             htmlMessage.remove()
         }
@@ -130,95 +132,91 @@ Hooks.on("renderChatMessage", (chatMessage, html, data) => {
 
 Hooks.on("preDeleteChatMessage", (chatMessage, option) => {
     //Check pinned message on the flush chat button
-    if(game.user.isGM
+    if (game.user.isGM
         && game.settings.get(s_MODULE_ID, "protectPinnedFromDeletion")
-        && chatMessage.flags?.pinnedChat?.pinned?.length > 0){
-            return false 
+        && chatMessage.flags?.pinnedChat?.pinned?.length > 0) {
+        return false
     }
 });
 
 Hooks.on('getChatLogEntryContext', (_chatLogApp, entries) => {
     const getmessage = (li) => game.messages.get(li.data('messageId'))
-  
+
     entries.unshift(
-      {
-        name:  game.i18n.localize('PCM.allPin'),
-        icon: '<i class="fas fa-map-pin"></i>',
-        condition: (li) => {
-            if(!game.user.isGM && game.settings.get(s_MODULE_ID, 'disablePinForAll')) return false
+        {
+            name: game.i18n.localize('PCM.allPin'),
+            icon: '<i class="fas fa-map-pin"></i>',
+            condition: (li) => {
+                if (!game.user.isGM && game.settings.get(s_MODULE_ID, 'disablePinForAll')) return false
 
-            const chatMessage = getmessage(li);
-            return checkIsPinned(chatMessage) !== ENUM_IS_PINNED_VALUE.all && allowToPinMessage(chatMessage);
-        },
-        callback: async (li) => {
-          const chatMessage = getmessage(li);
-          pinnedMessage(chatMessage)
-        }
-      },
-      {
-        name:  game.i18n.localize('PCM.selfPin'),
-        icon: '<i class="fas fa-map-pin"></i>',
-        condition: (li) => {
-            if(!game.user.isGM && game.settings.get(s_MODULE_ID, 'disableSelfPin')) return false
-
-            const chatMessage = getmessage(li);
-            return checkIsPinned(chatMessage) !== ENUM_IS_PINNED_VALUE.self && allowToPinMessage(chatMessage);
-        },
-        callback: async (li) => {
-            const chatMessage = getmessage(li);
-            pinnedMessage(chatMessage, {target : game.user.id})
-        },
-      },
-      {
-        name: game.i18n.localize('PCM.unPin'),
-        icon: '<i class="fas fa-map-pin"></i>',
-        condition: (li) => {
-            const chatMessage = getmessage(li);
-            const pinnedStatus = checkIsPinned(chatMessage)
-
-            const disablePin = !game.user.isGM
-                && (
-                    (pinnedStatus === ENUM_IS_PINNED_VALUE.all && game.settings.get(s_MODULE_ID, 'disablePinForAll'))
-                    || (pinnedStatus === ENUM_IS_PINNED_VALUE.self && game.settings.get(s_MODULE_ID, 'disableSelfPin'))
-                )
-
-            return !disablePin && pinnedStatus !== ENUM_IS_PINNED_VALUE.none && allowToPinMessage(chatMessage);
-        },
-        callback: async (li) => {
-            const chatMessage = getmessage(li);
-            if(checkIsPinned(chatMessage) === ENUM_IS_PINNED_VALUE.self){
-                pinnedMessage(chatMessage, {target : game.user.id, active:false})
-            } else if (checkIsPinned(chatMessage) === ENUM_IS_PINNED_VALUE.all){
-                pinnedMessage(chatMessage, {target : PINNED_FOR_ALL, active:false})
+                const chatMessage = getmessage(li);
+                return checkIsPinned(chatMessage) !== ENUM_IS_PINNED_VALUE.all && allowToPinMessage(chatMessage);
+            },
+            callback: async (li) => {
+                const chatMessage = getmessage(li);
+                pinnedMessage(chatMessage)
             }
         },
-      }
-    );
-  });
-  
-  /**
-  * Provides the main incoming message registration and distribution of socket messages on the receiving side.
-  */
-  function listenSocket()
-  {
-     game.socket.on(s_EVENT_NAME, (data) =>
-     {
-        if (typeof data !== 'object') { return; }
-  
-        //Only GM must update the chatMessage
-        if(!game.user.isGM){ return; }
-  
-        try
         {
-           if (data.type === 'pinnedUnownedMessage' && data?.payload?.messageId) {
-            const chatMessage = ChatMessage.get(data.payload.messageId)
+            name: game.i18n.localize('PCM.selfPin'),
+            icon: '<i class="fas fa-map-pin"></i>',
+            condition: (li) => {
+                if (!game.user.isGM && game.settings.get(s_MODULE_ID, 'disableSelfPin')) return false
 
-            pinnedMessageUpdate(chatMessage, data?.payload?.pinnedFor)
-           }
-        }
-        catch (err)
+                const chatMessage = getmessage(li);
+                return checkIsPinned(chatMessage) !== ENUM_IS_PINNED_VALUE.self && allowToPinMessage(chatMessage);
+            },
+            callback: async (li) => {
+                const chatMessage = getmessage(li);
+                pinnedMessage(chatMessage, { target: game.user.id })
+            },
+        },
         {
-           console.error(err);
+            name: game.i18n.localize('PCM.unPin'),
+            icon: '<i class="fas fa-map-pin"></i>',
+            condition: (li) => {
+                const chatMessage = getmessage(li);
+                const pinnedStatus = checkIsPinned(chatMessage)
+
+                const disablePin = !game.user.isGM
+                    && (
+                        (pinnedStatus === ENUM_IS_PINNED_VALUE.all && game.settings.get(s_MODULE_ID, 'disablePinForAll'))
+                        || (pinnedStatus === ENUM_IS_PINNED_VALUE.self && game.settings.get(s_MODULE_ID, 'disableSelfPin'))
+                    )
+
+                return !disablePin && pinnedStatus !== ENUM_IS_PINNED_VALUE.none && allowToPinMessage(chatMessage);
+            },
+            callback: async (li) => {
+                const chatMessage = getmessage(li);
+                if (checkIsPinned(chatMessage) === ENUM_IS_PINNED_VALUE.self) {
+                    pinnedMessage(chatMessage, { target: game.user.id, active: false })
+                } else if (checkIsPinned(chatMessage) === ENUM_IS_PINNED_VALUE.all) {
+                    pinnedMessage(chatMessage, { target: PINNED_FOR_ALL, active: false })
+                }
+            },
         }
-     });
-  }
+    );
+});
+
+/**
+* Provides the main incoming message registration and distribution of socket messages on the receiving side.
+*/
+function listenSocket() {
+    game.socket.on(s_EVENT_NAME, (data) => {
+        if (typeof data !== 'object') { return; }
+
+        //Only GM must update the chatMessage
+        if (!game.user.isGM) { return; }
+
+        try {
+            if (data.type === 'pinnedUnownedMessage' && data?.payload?.messageId) {
+                const chatMessage = ChatMessage.get(data.payload.messageId)
+
+                pinnedMessageUpdate(chatMessage, data?.payload?.pinnedFor)
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    });
+}
